39a3ea271796e30c3236257f1bae18eab6d0714d
ting1065 <fengtingtang1065@gmail.com>
2024-02-02 01:52:14 -0800

    finished image pre-process operations

--- sky_identification.py
+++ sky_identification.py
@@ -5,8 +5,21 @@ from matplotlib import pyplot as plt
 
 
 def mark_sky(img):
-    img = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
-    return img
+    # Convert to grayscale
+    img_gray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
+
+    # Apply Gaussian blur
+    img_blurred = cv.GaussianBlur(img_gray, (21, 5), 0)
+
+    # Use Adaptive Gaussian Thresholding to get a binary mask
+    img_binary = cv.adaptiveThreshold(img_blurred,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY,11,2)
+
+    # apply morphological operation opening to remove noise
+    # erosion and dilation, twice
+    kernel = np.ones((6, 6), np.uint8)
+    img_opened = cv.morphologyEx(img_binary, cv.MORPH_OPEN, kernel, iterations=2)
+
+    return img_opened
 
 # Gradio interface
 demo = gr.Interface(
